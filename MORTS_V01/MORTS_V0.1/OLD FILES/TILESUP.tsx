152e60d6c4ee9072815b8346d29f5a3f62af7d11
HospesSTE <krzysztof.jedowski@pipelife.com>
2021-03-11 15:02:15 +0100

    first commit

---
+++ MORTS_V01/MORTS_V0.1/OLD FILES/TILESUP.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="TILESUP" tilewidth="16" tileheight="8" tilecount="500" columns="20">
+ <image source="TILESUP.png" trans="22ff00" width="320" height="200"/>
+</tileset>
---
+++ MORTS_V01/MORTS_V0.1/PROCESS/GRIDTILE.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="GRIDTILE" tilewidth="8" tileheight="8" tilecount="1000" columns="40">
+ <image source="GRIDTILE.png" trans="000000" width="320" height="200"/>
+</tileset>
